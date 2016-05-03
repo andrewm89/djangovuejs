4d9cd74e17122065511e8a84bf43c194df1e3f67
Andrew McCracken <andrewm@theodo.fr>
2016-05-03 21:22:21 +0200

    Add Django REST framework

--- djangovuejs/urls.py
+++ djangovuejs/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from joblist.views import job_list
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', job_list)
+    url(r'^$', job_list),
+    url(r'^api-auth/', include('rest_framework.urls')),
 ]
